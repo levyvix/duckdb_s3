f350e0487a0e27ac48855c18e434c447f2f084fc
levyvix <levy.vix@gmail.com>
2024-10-19 22:16:24 -0300

    Add initial project files and dependencies

---
+++ hello.py
@@ -0,0 +1,9 @@
+import duckdb
+
+
+def main():
+    print("Hello from duckdb-s3!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ ingest/main.py
@@ -0,0 +1,13 @@
+import datetime as dt
+from datetime import datetime, timedelta
+
+from data_lake_ingester import DataLakeIngester
+
+ingester = DataLakeIngester("gharchive/events")
+
+now = datetime.now(dt.timezone.utc)
+
+# 2024-10-20 00:00:00+00:00
+process_date = now.replace(minute=0, second=0, microsecond=0) - timedelta(hours=2)
+
+ingester.ingest_hourly_gharchive(process_date)
